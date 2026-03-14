02f99ebfc076920eb66551a4022f24373b9e8e93
bruna-ramos-dev <bruna-chel@hotmail.com>
2026-03-14 15:19:25 -0300

    Versão 14-03-26

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,22 +1,101 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+    // ObservableCollection usada para atualizar a interface automaticamente
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
     public ListaProduto()
     {
         InitializeComponent();
+
+        // Liga a lista à interface
+        lst_produtos.ItemsSource = lista;
     }
 
+    // Carrega os produtos ao abrir a tela
+    protected async override void OnAppearing()
+    {
+        try
+        {
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
+
+    // Botão para abrir tela de cadastro de produto
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
         try
         {
             Navigation.PushAsync(new Views.NovoProduto());
-
         }
         catch (Exception ex)
         {
             DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    // Busca dinâmica com SearchBar
+    private async void Txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string q = e.NewTextValue;
+
+            lista.Clear();
+
+            List<Produto> tmp;
+
+            if (string.IsNullOrWhiteSpace(q))
+                tmp = await App.Db.GetAll();
+            else
+                tmp = await App.Db.Search(q);
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
+
+    // Soma total dos produtos
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    // Excluir produto
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem mi = sender as MenuItem;
+
+            Produto selecionado = mi.BindingContext as Produto;
+
+            await App.Db.Delete(selecionado.Id);
+
+            lista.Remove(selecionado);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
         }
     }
 }
